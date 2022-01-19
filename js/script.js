36535b18c5a4d27f8f6b2047f54dac300137b948
Nikita Safonkin <sliverregulyar@gmail.com>
2022-01-19 14:19:44 +0300

    black screen fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const $MainWin = document.querySelector(".MainWin");
-
+//$MainWin.scrollHeight
 
 const $AddLinkInput = document.querySelector("#AddLinkInput");
 const $AddDescriptionInput = document.querySelector("#AddDescriptionInput");
@@ -12,48 +12,44 @@ const $SHOTTIME = document.querySelector('.shotTime');
 
 const $BLACK = document.querySelector('.BlackScreen');
 
-
-const blackScreeToggle = (e) => {!e?$BLACK.classList.add("BlackOff"):$BLACK.classList.remove("BlackOff")};
+//               // BLACK SCREEEN
+const blackScreeToggle = (e) => { 
+    !e ? $BLACK.classList.add("BlackOff") : $BLACK.classList.remove("BlackOff");
+    $BLACK.style.height=$MainWin.scrollHeight+"px";
+};
 
 
 
 //              ADD INPUT RIGHT-CLICK PASTE CLIPBOARD
 
-const pasteClipboard=e=>navigator.clipboard.readText().then(text=>e.target.value=(text==''||text==' '||text==null||text==NaN)?"Скопируй текст":text);
+const pasteClipboard = e => navigator.clipboard.readText().then(text => e.target.value = (text == '' || text == ' ' || text == null || text == NaN) ? "Скопируй текст" : text);
 
-$AddLinkInput.addEventListener('contextmenu',e=>pasteClipboard(e));
+$AddLinkInput.addEventListener('contextmenu', e => pasteClipboard(e));
 
-$AddDescriptionInput.addEventListener('contextmenu',e=>pasteClipboard(e));
-    //e.target.value="click";
-$SHOTTIME.addEventListener('contextmenu',e=>pasteClipboard(e))
-
-// let clip='';
-// window.focus&&navigator.clipboard.readText().then(text=>$CLIP_STATUS.innerHTML=text);
+$AddDescriptionInput.addEventListener('contextmenu', e => pasteClipboard(e));
+//e.target.value="click";
+$SHOTTIME.addEventListener('contextmenu', e => pasteClipboard(e))
 
 
-
-
-// setInterval(()=>{
-//     navigator.clipboard.readText().then(text=>$CLIP_STATUS.innerHTML=text);
-
-// },1000);
-//$CLIP_STATUS.innerHTML="dsd";
 
 
 
 
 
 //-----------------------------------------------      RENDER SPIN WIN 
-function showBall(id, elOfArr) {
+function showBall(id, elOfArr,event) {
     blackScreeToggle(true);
+    console.log(event.pageX,event.pageY);
 
     $BALL.style.display = "block";// SHOW BALL WINDOW
-    $BALL.style.left='50%';
+
+    $BALL.style.left =event.pageX-70+"px";
+    $BALL.style.top =event.pageY-85+"px";
+    
     $BALL.style.zIndex = '10';
     const $AIM_BLOCKS = document.querySelectorAll('.aim_block'); // SELECT ALL BLOCKS 
 
     function aimSelect() {
-
         addAim(id, this.id, elOfArr); // ADD SELECTED SPIN ON ARRAY & LS        
         $BALL.style.display = "none"; // HIDE BALL DIV
         Array.from($AIM_BLOCKS).map(item => { // CLEAR LISTENERS ON BALL BLOCKS
@@ -95,11 +91,11 @@ const textSpin = spin => {
 //GET RANDOM ID
 const rndID = (name = "item") => `id_${name}_${Math.random()}`;
 
-//CLICKS CHECKER
+//---------------------------------LEFT-CLICKS CHECKER
 document.addEventListener('click', e => {
     e.target.name == "del" && delItem(e.target.id); // del buttons
     //так находим элемент alt в котором передаем идекс в массиве...
-    e.target.name == "aim" && showBall(e.target.id, e.target.attributes.alt.nodeValue);// AIM buttons
+    e.target.name == "aim" && showBall(e.target.id, e.target.attributes.alt.nodeValue,e);// AIM buttons
     e.target.name == "shot" && addShotTime(e);// AIM buttons
     e.target.name == "del_shot" && delShot(e.target.id, e.target.attributes.alt.nodeValue);// DEL SHOT
 })
@@ -107,20 +103,17 @@ document.addEventListener('click', e => {
 
 
 
+//-------------------------------- RIGHT-CLICKS CHECKER
+document.addEventListener('contextmenu', e => {
 
-// RIGHT CLICK ADD TIME SHOT
-document.addEventListener('contextmenu',e=>{
-e.target.name=="shot"&&(async()=>{
-    let clip='none';
-    await navigator.clipboard.readText().then(text => clip=text);
+    e.target.name == "shot" && (async () => {
+        let clip = 'none';
+        await navigator.clipboard.readText().then(text => clip = text);
+        addShotArr(e.target.id, clip);
 
-    addShotArr(e.target.id,clip);
-    console.log("RIGHT CLICK ON ADD_SHOT");
-})();
+    })();
 
 })
-
-
 
 
 
@@ -130,7 +123,7 @@ function addShotTime(e) {
     blackScreeToggle(true);
     $SHOTTIME.style.zIndex = '10';
     $SHOTTIME.style.display = 'block';
-    $SHOTTIME.style.top = e.clientY + 'px';
+    $SHOTTIME.style.top = e.pageY + 'px';
     $SHOTTIME.style.left = e.clientX + 'px';
 
 
@@ -170,8 +163,8 @@ listRender();
 function addItem(link, description) {
     listArr.push({ ID: rndID(), link, description });
 
-    $AddLinkInput.value='';
-    $AddDescriptionInput.value='';
+    $AddLinkInput.value = '';
+    $AddDescriptionInput.value = '';
     listRender();
     setST();
 }
@@ -218,6 +211,8 @@ function delShot(idVideo, elOfArr) {
 
 
 
+
+
 // ADD SHOT FUNC
 
 function addShotArr(ID, textTime) {
@@ -225,9 +220,7 @@ function addShotArr(ID, textTime) {
     const obj = listArr.find(item => item.ID == ID);// find obj in arr
     const itemIndex = listArr.indexOf(obj);// get index of item in array
     listArr[itemIndex].shots ?? (listArr[itemIndex].shots = []);
-    listArr[itemIndex].shots.push({ time: textTime, aim: null });
-    // console.log(listArr[itemIndex].shots);
-    // console.log(listArr);
+    listArr[itemIndex].shots.push({ time: textTime, aim: null, color: "white" });
 
     blackScreeToggle(false);
     listRender(); //update list  
@@ -236,7 +229,7 @@ function addShotArr(ID, textTime) {
 
 
 
-//00 час 26 мин 23 сек
+
 // 00 час : 06 мин : 48 сек
 //original link https://www.youtube.com/watch?v=sWd1qoZ-DNg
 //time link     https://youtu.be/sWd1qoZ-DNg?t=14
@@ -244,52 +237,26 @@ function addShotArr(ID, textTime) {
 // -------------------------  CALC VIDEO TIMECODE
 function calcTimeCode(videoLink, textCode) {
     let arr = textCode.split(' ');
-    console.log(arr[0], arr[2], arr[4]);
-    console.log("ARR",arr);
     let secs = (+arr[0] * 60 * 60) + (+arr[3] * 60) + (+arr[6]);
-
-    
-    let videoName=videoLink.split('=');
-    
-
-    return `https://youtu.be/${videoName[1]}?t=${secs}s`
-    
+    let videoName = videoLink.split('=');
+    return `https://youtu.be/${videoName[1]}?t=${secs}s`;
 }
 
 
-
-
-
-
-
-
-// https://www.youtube.com/watch?v=bVEjOd6_y4Y
-// https://youtu.be/bVEjOd6_y4Y?t=950
-
-
-// ----------------------SHOTS LIST 
+// -------------------------------------------SHOTS LIST 
 function addShotsList(link, id, arr, item) {
-    //  console.log(link,id,arr);
     let $items = document.querySelector('#' + id);
-    console.log($items);
     arr && arr.map((el, index) => {
-
         $items.innerHTML += `
-    <div class="shot_item"> 
-    
+    <div class="shot_item" id="${item.ID}" name="shot_item">     
     <button id="${item.ID}" alt="${index}" name="del_shot">X</button>  
-
-    <a href="${calcTimeCode(link,el.time)}" target="_blank">${el.time}</a>
-    
-    <button id="${item.ID}" alt="${index}" name="aim" ${textSpin(item.shots[index].aim)||"style=background:red"}> 🎯</button>   
-     ${textSpin(item.shots[index].aim) || "..."}
-   
+    <a href="${calcTimeCode(link, el.time)}" target="_blank">${el.time}</a>    
+    <button id="${item.ID}" alt="${index}" name="aim" ${textSpin(item.shots[index].aim) || "style=background:red"}> 🎯</button>   
+     ${textSpin(item.shots[index].aim) || "..."}   
     </div>
     `}
     )
 }
-
-
 
 
 //-------------------------------------------LIST RENDER  FUNC (from array)
@@ -297,8 +264,6 @@ function listRender() {
     $ListItems.innerHTML = '';
     listArr.map((item, index) => {
         $ListItems.innerHTML += // ITEM HTML
-
-            // <button id="${item.ID}" name="aim">AIM</button>
             `
     <div class="itemOfList">
     
